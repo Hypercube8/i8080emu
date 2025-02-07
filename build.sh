dfade93238e209709154ee400153bc6415a1f08b
Hypercube8 <146479533+Hypercube8@users.noreply.github.com>
2025-02-07 23:58:23 +0000

    Profiling

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ if test -f "output/i8080emu" ;
 then
 rm output/i8080emu
 fi
-gcc src/* -Iinclude -O3 -o output/i8080emu -DDEBUG
+gcc src/* -Iinclude -O3 -o output/i8080emu -pg -DDEBUG
 ./output/i8080emu
