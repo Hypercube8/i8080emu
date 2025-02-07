fc6ed062541dcd7ae61e3242de0aa1f2a6b9e0d8
Hypercube8 <146479533+Hypercube8@users.noreply.github.com>
2025-02-07 23:09:03 +0000

    Comprehensive testing

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ if test -f "output/i8080emu" ;
 then
 rm output/i8080emu
 fi
-gcc src/* -Iinclude -o output/i8080emu -DDEBUG
+gcc src/* -Iinclude -O3 -o output/i8080emu -DDEBUG
 ./output/i8080emu
--- src/test.c
+++ src/test.c
@@ -1,5 +1,20 @@
 #include "i8080.h"
 #include <stdlib.h>
+#include <time.h>
+
+enum bdos_syscalls {
+    P_TERMCPM = 0x00,
+    C_WRITE = 0x01,
+    C_WRITECHAR = 0x02,
+    C_WRITESTR = 0x09
+};
+
+typedef struct {
+    size_t ins;
+    size_t cycles;
+    double time;
+    double speed;
+} test_results_t;
 
 uint8_t *memptr;
 bool finished = false;
@@ -7,6 +22,11 @@ i8080_cpu_t cpu;
 
 void load_file(const char *path) {
     FILE *f = fopen(path, "rb");
+
+    if (f == NULL) {
+        perror("Failed to open file");
+        return;
+    }
 
     fseek(f, 0, SEEK_END);
     size_t length = ftell(f);
@@ -30,17 +50,17 @@ uint8_t inb(void *userptr, uint8_t port) {
 
 void outb(void *userptr, uint8_t port, uint8_t val) {
     switch (port) {
-        case 0: {
+        case P_TERMCPM: {
             finished = true;
             break;
         }
-        case 1:
+        case C_WRITE:
             switch (cpu.c) {
-                case 2: {
+                case C_WRITECHAR: {
                     printf("%c", cpu.e);
                     break;
                 }
-                case 9: {
+                case C_WRITESTR: {
                     uint16_t addr = cpu.de;
                     do {
                         printf("%c", rb(NULL, addr++));
@@ -51,32 +71,54 @@ void outb(void *userptr, uint8_t port, uint8_t val) {
     }
 }
 
-int main() {
-    size_t ins;
+void log_results(const char* test_name, test_results_t *results) {
+    printf("\nTEST %s RESULTS", test_name);
+    printf("\n\tInstructions Executed: %ld", results->ins);
+    printf("\n\tCycles Executed: %ld", results->cycles);
+    printf("\n\tTime Elapsed: %f", results->time);
+    printf("\n\tSpeed (MIPS): %f", results->speed);
+}
+
+void run_test(const char *path) {
+    test_results_t results = {0};
+    printf("\nBEGINNING TEST %s\n", path);
+    finished = false;
+
     memptr = malloc(0x10000 * sizeof(uint8_t));
-    load_file("roms/8080EXM.COM");
+    if (memptr == NULL) {
+        perror("Failed to allocate memory");
+        return 1;
+    }
+
+    load_file(path);
+
     memptr[RESTART_0+0] = OUT_D8;
     memptr[RESTART_0+1] = 0x00;
     memptr[RESTART_0+5] = OUT_D8;
     memptr[RESTART_0+6] = 0x01;
     memptr[RESTART_0+7] = RET;
-    
     i8080_init(&cpu, &cpu, rb, wb, inb, outb);
     cpu.pc = 0x100;
+
+    clock_t begin = clock();
     while (!cpu.hlt && !finished) {
-        // #if __linux__
-        //     system("clear");
-        // #elif _WIN32
-        //     system("cls");
-        // #endif
         i8080_step(&cpu);
-        // i8080_dump_registers(&cpu);
-        //i8080_dump_memory(&cpu, 0x00);
-        //i8080_dump_stack(&cpu);
-        //fgetc(stdin);
-        ins++;
+        results.ins += 1;
     }
-    printf("\nInstructions: %ld Cycles: %ld \n", ins, cpu.cycles);
+    clock_t end = clock();
+
+    results.time = (double)(end - begin) / CLOCKS_PER_SEC;
+    results.speed = (results.ins / results.time) / 1000000;
+    results.cycles = cpu.cycles;
+
     free(memptr);
+    log_results(path, &results);
+}
+
+int main() {
+    run_test("roms/8080PRE.COM");
+    run_test("roms/TST8080.COM");
+    run_test("roms/CPUTEST.COM");
+    run_test("roms/8080EXM.COM");
     return 0;
 }
